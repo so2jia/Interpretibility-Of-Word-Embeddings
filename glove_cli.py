ec0923fa2118326982f0c32fb6ba85ea45dc14ee
Tamás Ficsor <ficstamas@gmail.com>
2019-09-07 13:30:57 +0200

    Load flag to Glove CLI and README update

--- glove_cli.py
+++ glove_cli.py
@@ -12,10 +12,12 @@ if __name__ == '__main__':
     parser.add_argument("--lambda_value", type=int, required=False, default=1,
                         help="Lambda value for relaxing score computation")
     parser.add_argument("--weights_dir", type=str, required=False,
-                        help="The directory where weight matrices loaded from and saved to "
-                             "(f.e. I.npy, wb.npy, wbs.npy)")
+                        help="The directory where weight matrices loaded from and saved to"
+                             "(f.e. I.npy, wb.npy, wbs.npy). Default is \"out/\", if not provided.")
     parser.add_argument("--save", action="store_true", required=False,
-                        help="Flag whether you want to save the matrices or not")
+                        help="Flag whether you want to save the matrices or not.")
+    parser.add_argument("--load", action="store_true", required=False,
+                        help="Flag whether you want to load the matrices or not.")
 
     args = parser.parse_args()
 
@@ -26,7 +28,8 @@ if __name__ == '__main__':
 
     weights_dir = args.weights_dir
     save = args.save
+    load = args.load
 
     model = Glove(embedding_path, semcat_dir, weights_dir="out" if weights_dir is None else weights_dir,
-                  save=save, load=weights_dir is not None)
+                  save=save, load=load)
     model.calculate_score(lamb)
