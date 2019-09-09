652acd9c33dc818aa45b337345df59e1745c1fcd
Tamás Ficsor <ficstamas@gmail.com>
2019-09-09 15:39:48 +0200

    Categorical decomposition

--- Plots/categorical_decomposation.py
+++ Plots/categorical_decomposation.py
@@ -1,19 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from argparse import ArgumentParser
 
-w = np.load("../out/w_nsb.npy")
 
-dim = 2
+def plot_categorical_decomposition(input_file, dimenstion, output_file=None):
+    """
+    Plot categorical decomposition
+    Parameters
+    ----------
+    input_file: str
+        Path to Category-Weight matrix (w_b.npy)
+    dimenstion: int
+        The dimension of decomposition
+    output_file
+        Output PNG file (Optional)
+    Returns
+    -------
 
-selected_dim = w[dim, :]
+    """
+    w = np.load(input_file)
 
-# objects = np.arange(len(selected_dim))
-# y_pos = np.arange(len(selected_dim))
-#
-#
-# plt.bar(y_pos, selected_dim, align='center', alpha=0.5)
-# plt.xticks(y_pos, objects)
-# plt.ylabel('Usage')
-# plt.title('Programming language usage')
-#
-# plt.show()
+    dim = dimenstion
+
+    selected_dim = w[dim, :]
+
+    objects = range(0, len(selected_dim), 10)
+    y_pos = np.arange(len(selected_dim))
+
+    plt.bar(y_pos, selected_dim, align='center', alpha=0.5)
+    plt.ylabel('Weight')
+    plt.xlabel('Categories')
+    plt.title(f'Categorical Decomposition of {dim}. dimension')
+
+    if output_file is not None:
+        plt.savefig(output_file)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Glove interpretibility')
+
+    # Required parameters
+    parser.add_argument("input_file", type=str,
+                        help="Bhattacharya distance matrix (w_b.npy)")
+    parser.add_argument("dimension", type=int,
+                        help="The dimension of decomposition")
+    parser.add_argument("--output_file", type=str,
+                        help="Output PNG file (Optional)")
+
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    dimension = args.dimension
+    output_file = args.output_file
+
+    plot_categorical_decomposition(input_file, dimension, output_file)
