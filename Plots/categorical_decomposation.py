779368ddad4144eaffeef26c2657087f18e8a82d
Tamás Ficsor <ficstamas@gmail.com>
2019-09-09 13:28:20 +0200

    Categorical decomposition (not done)

--- Plots/categorical_decomposation.py
+++ Plots/categorical_decomposation.py
@@ -2,4 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 w = np.load("../out/w_nsb.npy")
-print(w)
+
+dim = 2
+
+selected_dim = w[dim, :]
+
+# objects = np.arange(len(selected_dim))
+# y_pos = np.arange(len(selected_dim))
+#
+#
+# plt.bar(y_pos, selected_dim, align='center', alpha=0.5)
+# plt.xticks(y_pos, objects)
+# plt.ylabel('Usage')
+# plt.title('Programming language usage')
+#
+# plt.show()
