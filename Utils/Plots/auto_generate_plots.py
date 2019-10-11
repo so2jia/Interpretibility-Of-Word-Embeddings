e629f32cb61a8cae3a7a3002da3df78d8ee09798
Tamás Ficsor <ficstamas@gmail.com>
2019-10-11 14:31:15 +0200

    full vocab based bhatta

--- Utils/Plots/auto_generate_plots.py
+++ Utils/Plots/auto_generate_plots.py
@@ -12,15 +12,15 @@ line_to_read = 50000
 python = "/home/tamas/repos/Interpretibility-Of-Word-Embeddings/venv/bin/python"
 
 embedding = f"../../data/glove/glove.6B.300d.txt"
-e_s = f"../../out/kde/sparse_model/semantic/l_0.{reg}_e_s.npy"
+e_s = f"../../out/exponential/dense_e_s.npy"
 
 for m in mat:
     for n in nor:
         norm = n
-        w = f"../../out/kde/sparse_model/semantic/l_0.{reg}_{m}.npy"
-        output_file = f"../../out/kde/sparse_results/semantic/l0{reg}/l_0.{reg}_{m}.png"
+        w = f"../../out/exponential/dense_{m}.npy"
+        output_file = f"../../out/exponential/result/dense_{m}.png"
         args = [python, 'interpretability_correlation.py',
-                embedding, e_s, w, semcat, norm_type,
+                embedding, e_s, w, semcat, norm_type, "-dense",
                 "--lamb=10", f"--output_file={output_file}"]
         if n:
             args.append("-norm")
--- Utils/Plots/is_lineplot.py
+++ Utils/Plots/is_lineplot.py
@@ -4,28 +4,28 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-paths = [["kde/dense_results/dense_w_b_10-stats.txt", "W_b"],
-         ["kde/dense_results/dense_w_nb_10-stats.txt", "W_nb"],
-         ["kde/dense_results/dense_w_nsb_10-stats.txt", "W_nsb"],
-         ["kde/dense_results/dense_w_b_10-norm-stats.txt", "W_b norm"],
-         ["kde/dense_results/dense_w_nb_10-norm-stats.txt", "W_nb norm"],
-         ["kde/dense_results/dense_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
+paths = [["dense_w_b_10-stats.txt", "W_b"],
+         ["dense_w_nb_10-stats.txt", "W_nb"],
+         ["dense_w_nsb_10-stats.txt", "W_nsb"],
+         ["dense_w_b_10-norm-stats.txt", "W_b norm"],
+         ["dense_w_nb_10-norm-stats.txt", "W_nb norm"],
+         ["dense_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
 
-paths2 = [["kde/sparse_results/raw/l0%/l0%_w_b_10-stats.txt", "W_b"],
-          ["kde/sparse_results/raw/l0%/l0%_w_nb_10-stats.txt", "W_nb"],
-          ["kde/sparse_results/raw/l0%/l0%_w_nsb_10-stats.txt", "W_nsb"],
-          ["kde/sparse_results/raw/l0%/l0%_w_b_10-norm-stats.txt", "W_b norm"],
-          ["kde/sparse_results/raw/l0%/l0%_w_nb_10-norm-stats.txt", "W_nb norm"],
-          ["kde/sparse_results/raw/l0%/l0%_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
+# paths2 = [["kde/sparse_results/raw/l0%/l0%_w_b_10-stats.txt", "W_b"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_nb_10-stats.txt", "W_nb"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_nsb_10-stats.txt", "W_nsb"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_b_10-norm-stats.txt", "W_b norm"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_nb_10-norm-stats.txt", "W_nb norm"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
+#
+# paths3 = [["kde/sparse_results/semantic/l0%/l_0.%_w_b_10-stats.txt", "W_b"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_nb_10-stats.txt", "W_nb"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_nsb_10-stats.txt", "W_nsb"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_b_10-norm-stats.txt", "W_b norm"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_nb_10-norm-stats.txt", "W_nb norm"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
 
-paths3 = [["kde/sparse_results/semantic/l0%/l_0.%_w_b_10-stats.txt", "W_b"],
-          ["kde/sparse_results/semantic/l0%/l_0.%_w_nb_10-stats.txt", "W_nb"],
-          ["kde/sparse_results/semantic/l0%/l_0.%_w_nsb_10-stats.txt", "W_nsb"],
-          ["kde/sparse_results/semantic/l0%/l_0.%_w_b_10-norm-stats.txt", "W_b norm"],
-          ["kde/sparse_results/semantic/l0%/l_0.%_w_nb_10-norm-stats.txt", "W_nb norm"],
-          ["kde/sparse_results/semantic/l0%/l_0.%_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
-
-prefix = "../../out/"
+prefix = "../../out/exponential/result/"
 
 base_path = os.path.join(os.getcwd(), prefix)
 
@@ -38,47 +38,47 @@ for path in paths:
 
 data = pd.DataFrame(data=values, index=np.arange(1, 11, 1))
 
-fig, axes = plt.subplots(3, 4, sharey=True, sharex=True)
+fig, axes = plt.subplots(1, 1, sharey=True, sharex=True)
 fig.suptitle("Interpretibility")
-fig.set_size_inches(20, 15)
+fig.set_size_inches(6, 6)
 
 
-ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axes.flat[0])
+ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axes)
 ax.set_xlabel("Lambda")
 ax.set_ylabel("IS' (%)")
-ax.set_title('Dense embedding space')
+ax.set_title('Dense (Exponential Kernel))')
 
-# sparse raw
-for i in range(5):
-    values = {}
-    for path in paths2:
-        p = os.path.join(base_path, path[0].replace("%", str(i+1)))
-        with open(p, mode="r", encoding="utf8") as f:
-            values[path[1]] = np.array(f.readlines()[3:], dtype=np.float)
-    data = pd.DataFrame(data=values, index=np.arange(1, 11, 1))
-    axs = axes.flat[i+1]
-    ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axs)
-    ax.set_xlabel("Lambda")
-    ax.set_ylabel("IS' (%)")
-    ax.set_title(f'Sparse embedding space l=0.{i+1}')
-
-# sparse semantic
-for i in range(5):
-    values = {}
-    for path in paths3:
-        p = os.path.join(base_path, path[0].replace("%", str(i+1)))
-        with open(p, mode="r", encoding="utf8") as f:
-            values[path[1]] = np.array(f.readlines()[3:], dtype=np.float)
-    data = pd.DataFrame(data=values, index=np.arange(1, 11, 1))
-    axs = axes.flat[5+i+1]
-    ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axs)
-    ax.set_xlabel("Lambda")
-    ax.set_ylabel("IS' (%)")
-    ax.set_title(f'Sparse semantic space l=0.{i+1}')
+# # sparse raw
+# for i in range(5):
+#     values = {}
+#     for path in paths2:
+#         p = os.path.join(base_path, path[0].replace("%", str(i+1)))
+#         with open(p, mode="r", encoding="utf8") as f:
+#             values[path[1]] = np.array(f.readlines()[3:], dtype=np.float)
+#     data = pd.DataFrame(data=values, index=np.arange(1, 11, 1))
+#     axs = axes.flat[i+1]
+#     ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axs)
+#     ax.set_xlabel("Lambda")
+#     ax.set_ylabel("IS' (%)")
+#     ax.set_title(f'Sparse embedding space l=0.{i+1}')
+#
+# # sparse semantic
+# for i in range(5):
+#     values = {}
+#     for path in paths3:
+#         p = os.path.join(base_path, path[0].replace("%", str(i+1)))
+#         with open(p, mode="r", encoding="utf8") as f:
+#             values[path[1]] = np.array(f.readlines()[3:], dtype=np.float)
+#     data = pd.DataFrame(data=values, index=np.arange(1, 11, 1))
+#     axs = axes.flat[5+i+1]
+#     ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axs)
+#     ax.set_xlabel("Lambda")
+#     ax.set_ylabel("IS' (%)")
+#     ax.set_title(f'Sparse semantic space l=0.{i+1}')
 
 plt.xticks(np.arange(1, 11, 1))
 plt.xlim([1, 10])
 
-plt.savefig("../../out/kde/is.png")
+plt.savefig("../../out/exponential/result/is.png")
 
 plt.show()
