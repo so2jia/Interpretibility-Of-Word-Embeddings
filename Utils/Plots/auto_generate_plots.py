ee56d3043a8f32d10dd2641bd37295f0d2f25c2d
Tamás Ficsor <ficsamas@gmail.com>
2019-10-18 12:51:29 +0200

    Auto stash before checking out "HEAD"

--- Utils/Plots/auto_generate_plots.py
+++ Utils/Plots/auto_generate_plots.py
@@ -12,8 +12,13 @@ line_to_read = 50000
 python = "/home/tamas/repos/Interpretibility-Of-Word-Embeddings/venv/bin/python"
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 embedding = f"../../data/glove/glove.6B.300d.txt"
 e_s = f"../../out/kde/sparse_model/semantic/l_0.{reg}_e_s.npy"
+=======
+embedding = f"../../out/comp_semantic/comp_I.embedding.100d.txt"
+e_s = f"../../out/comp_semantic/comp_e_s.npy"
+>>>>>>> Stashed changes
 =======
 embedding = f"../../out/comp_semantic/comp_I.embedding.100d.txt"
 e_s = f"../../out/comp_semantic/comp_e_s.npy"
@@ -23,8 +28,13 @@ for m in mat:
     for n in nor:
         norm = n
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         w = f"../../out/kde/sparse_model/semantic/l_0.{reg}_{m}.npy"
         output_file = f"../../out/kde/sparse_results/semantic/l0{reg}/l_0.{reg}_{m}.png"
+=======
+        w = f"../../out/comp_semantic/comp_{m}.npy"
+        output_file = f"../../out/comp_semantic/result/comp_{m}.png"
+>>>>>>> Stashed changes
 =======
         w = f"../../out/comp_semantic/comp_{m}.npy"
         output_file = f"../../out/comp_semantic/result/comp_{m}.png"
--- Utils/Plots/is_lineplot.py
+++ Utils/Plots/is_lineplot.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 paths = [["kde/dense_results/dense_w_b_10-stats.txt", "W_b"],
          ["kde/dense_results/dense_w_nb_10-stats.txt", "W_nb"],
          ["kde/dense_results/dense_w_nsb_10-stats.txt", "W_nsb"],
@@ -12,6 +13,8 @@ paths = [["kde/dense_results/dense_w_b_10-stats.txt", "W_b"],
          ["kde/dense_results/dense_w_nb_10-norm-stats.txt", "W_nb norm"],
          ["kde/dense_results/dense_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
 =======
+=======
+>>>>>>> Stashed changes
 name_prefix = "dense_"
 
 paths = [[f"{name_prefix}w_b_10-stats.txt", "W_b"],
@@ -20,6 +23,7 @@ paths = [[f"{name_prefix}w_b_10-stats.txt", "W_b"],
          [f"{name_prefix}w_b_10-norm-stats.txt", "W_b norm"],
          [f"{name_prefix}w_nb_10-norm-stats.txt", "W_nb norm"],
          [f"{name_prefix}w_nsb_10-norm-stats.txt", "W_nsb norm"]]
+<<<<<<< Updated upstream
 >>>>>>> Stashed changes
 
 paths2 = [["kde/sparse_results/raw/l0%/l0%_w_b_10-stats.txt", "W_b"],
@@ -37,6 +41,24 @@ paths3 = [["kde/sparse_results/semantic/l0%/l_0.%_w_b_10-stats.txt", "W_b"],
           ["kde/sparse_results/semantic/l0%/l_0.%_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
 
 prefix = "../../out/"
+=======
+
+# paths2 = [["kde/sparse_results/raw/l0%/l0%_w_b_10-stats.txt", "W_b"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_nb_10-stats.txt", "W_nb"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_nsb_10-stats.txt", "W_nsb"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_b_10-norm-stats.txt", "W_b norm"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_nb_10-norm-stats.txt", "W_nb norm"],
+#           ["kde/sparse_results/raw/l0%/l0%_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
+#
+# paths3 = [["kde/sparse_results/semantic/l0%/l_0.%_w_b_10-stats.txt", "W_b"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_nb_10-stats.txt", "W_nb"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_nsb_10-stats.txt", "W_nsb"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_b_10-norm-stats.txt", "W_b norm"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_nb_10-norm-stats.txt", "W_nb norm"],
+#           ["kde/sparse_results/semantic/l0%/l_0.%_w_nsb_10-norm-stats.txt", "W_nsb norm"]]
+
+prefix = "../../out/exponential/result/"
+>>>>>>> Stashed changes
 
 base_path = os.path.join(os.getcwd(), prefix)
 
@@ -57,6 +79,7 @@ fig.set_size_inches(20, 15)
 ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axes.flat[0])
 ax.set_xlabel("Lambda")
 ax.set_ylabel("IS' (%)")
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 ax.set_title('Dense embedding space')
 
@@ -90,6 +113,37 @@ for i in range(5):
     ax.set_xlabel("Lambda")
     ax.set_ylabel("IS' (%)")
     ax.set_title(f'Sparse semantic space l=0.{i+1}')
+=======
+ax.set_title('Dense (Exponential))')
+
+# # sparse raw
+# for i in range(5):
+#     values = {}
+#     for path in paths2:
+#         p = os.path.join(base_path, path[0].replace("%", str(i+1)))
+#         with open(p, mode="r", encoding="utf8") as f:
+#             values[path[1]] = np.array(f.readlines()[3:], dtype=np.float)
+#     data = pd.DataFrame(data=values, index=np.arange(1, 11, 1))
+#     axs = axes.flat[i+1]
+#     ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axs)
+#     ax.set_xlabel("Lambda")
+#     ax.set_ylabel("IS' (%)")
+#     ax.set_title(f'Sparse embedding space l=0.{i+1}')
+#
+# # sparse semantic
+# for i in range(5):
+#     values = {}
+#     for path in paths3:
+#         p = os.path.join(base_path, path[0].replace("%", str(i+1)))
+#         with open(p, mode="r", encoding="utf8") as f:
+#             values[path[1]] = np.array(f.readlines()[3:], dtype=np.float)
+#     data = pd.DataFrame(data=values, index=np.arange(1, 11, 1))
+#     axs = axes.flat[5+i+1]
+#     ax = seaborn.lineplot(hue="Mathod", markers=True, dashes=False, data=data, ax=axs)
+#     ax.set_xlabel("Lambda")
+#     ax.set_ylabel("IS' (%)")
+#     ax.set_title(f'Sparse semantic space l=0.{i+1}')
+>>>>>>> Stashed changes
 
 plt.xticks(np.arange(1, 11, 1))
 plt.xlim([1, 10])
